7d5f26e4796ef4a0bb34dd28daa9647085fd58df
jkarrer <jkarrer@protect247.com>
2024-08-20 09:01:29 -0400

    add: vectorize the csv rows

--- README.md
+++ README.md
@@ -13,14 +13,18 @@ In this challenge, we ask you to build a predictive model that answers the quest
 It is your job to predict if a passenger survived the sinking of the Titanic or not.
 For each in the test set, you must predict a 0 or 1 value for the variable.
 
-## Metric
+### Metric
 
 Your score is the percentage of passengers you correctly predict. This is known as accuracy.
 
-## Submission File Format
+### Submission File Format
 
 You should submit a csv file with exactly 418 entries plus a header row. Your submission will show an error if you have extra columns (beyond PassengerId and Survived) or rows.
 The file should have exactly 2 columns:
 
 PassengerId (sorted in any order)
 Survived (contains your binary predictions: 1 for survived, 0 for deceased)
+
+## Data
+
+The dataset used in this challenge can be found [on kaggle](https://www.kaggle.com/competitions/titanic/data).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+// use plotly::{Plot, Scatter};
+
+use std::fs::File;
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct TitanicRecord {
+    PassengerId: u32,
+    Survived: u8,
+    Pclass: u8,
+    Name: String,
+    Sex: String,
+    Age: Option<f32>,
+    SibSp: u8,
+    Parch: u8,
+    Ticket: String,
+    Fare: f32,
+    Cabin: Option<String>,
+    Embarked: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // ** 1: Prep Data **
+    // raw csv to vector of records
+    let mut reader = csv::ReaderBuilder::new()
+        .from_path(std::path::Path::new("data/train.csv"))
+        .expect("could not read csv");
+
+    let records: Vec<TitanicRecord> = reader
+        .deserialize()
+        .map(|item| item.expect("not a valid record"))
+        .collect();
+
+    dbg!(&records[0]);
+
+    // dbg!(df);
+    // let mut plot = Plot::new();
+    // let trace = Scatter::new(vec![0, 1, 2], vec![2, 1, 0]);
+    // plot.add_trace(trace);
+
+    // plot.write_html("out.html");
 }
